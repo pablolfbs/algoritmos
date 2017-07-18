3694445e68c5d202768886702cf64a7a2d852be0
pablo <pablo@PABLO>
2017-07-18 16:55:33 -0300

    ffff

--- EstruturaFor/src/MainApp.java
+++ EstruturaFor/src/MainApp.java
@@ -1,12 +1,10 @@
 
 public class MainApp {
-	
+
 	public static void main(String[] args) {
-		
-		for (int i = 2; i <= 100; i=i+2) {
+
+		for (int i = 2; i <= 100; i = i + 2) {
 			System.out.println("Número: " + i);
 		}
-			
 	}
-
 }
