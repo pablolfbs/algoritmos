2297d86fdfd29baf5927190f03d78aa98f7ef7fe
Pablo <pablo.fariabritto@gmail.com>
2017-10-17 00:17:46 -0200

    Conclusão do projeto FibonacciRecursivo

--- FibonacciRecursivo/src/FibonacciMetodoRecursivo.java
+++ FibonacciRecursivo/src/FibonacciMetodoRecursivo.java
@@ -2,31 +2,30 @@
 public class FibonacciMetodoRecursivo {
 
 	public static void main(String[] args) {
-		
-		int[] a = new int[30];
-		a[0] = 0;
-		a[1] = 1;
-		int b = 0;
-		fibonacciMetodoRecursivo(b, a);
-		imprimeArray(a);
+//		int n = 0;
+//		fibonacciMetodoRecursivo(n);
+//		imprimeArray(n);
 //		System.out.println(fibonacciMetodoRecursivo(a[30]));
-
+		
+		int count = 1;
+		for (int i = 0; i < 40; i++) {
+			System.out.print(count++ + " - ");
+			System.out.println(fibonacciMetodoRecursivo(i));
+		}
 	}
 	
-	private static void imprimeArray(int[] a) {
-		for (int i = 0; i < a.length; i++) {
-			System.out.println(a[i]);
-		}
-	}
+//	private static void imprimeArray(int n) {
+//		
+//	}
 
-	private static int fibonacciMetodoRecursivo(int b, int[] a) {
-		if(b == 0) {
+	private static int fibonacciMetodoRecursivo(int n) {
+		if(n == 0) {
 			return 0;
 		}
-		if(b == 1) {
+		if(n == 1) {
 			return 1;
 		}
-		return (fibonacciMetodoRecursivo(b - 1, a[i -1]));
+		return (fibonacciMetodoRecursivo(n - 1) + fibonacciMetodoRecursivo(n - 2));
 		
 
 		
@@ -34,11 +33,11 @@ public class FibonacciMetodoRecursivo {
 //		a[0] = 0;
 //		a[1] = 1;
 //		int i = 2;
-		if(b < 30) {
-			a[b] += fibonacciMetodoRecursivo((b+1), a);
-		}
-		return fibonacciMetodoRecursivo((b+1), a);
-
+//		if(b < 30) {
+//			a[b] += fibonacciMetodoRecursivo((b+1), a);
+//		}
+//		return fibonacciMetodoRecursivo((b+1), a);
+//
 	}
 	
 
